0515b6974cd97bdcc5c9f4e15880a00e98b28b53
yso <yso1983@gmail.com>
2021-06-11 15:44:52 +0900

    First Commit

---
+++ DevExpress/XtraForm1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace DevExpress
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiGrid_Main = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.uiGrid_Main)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiGrid_Main
+            // 
+            this.uiGrid_Main.Location = new System.Drawing.Point(12, 12);
+            this.uiGrid_Main.MainView = this.gridView1;
+            this.uiGrid_Main.Name = "uiGrid_Main";
+            this.uiGrid_Main.Size = new System.Drawing.Size(1192, 836);
+            this.uiGrid_Main.TabIndex = 0;
+            this.uiGrid_Main.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.uiGrid_Main;
+            this.gridView1.Name = "gridView1";
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 860);
+            this.Controls.Add(this.uiGrid_Main);
+            this.Name = "XtraForm1";
+            this.Text = "XtraForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.uiGrid_Main)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XtraGrid.GridControl uiGrid_Main;
+        private XtraGrid.Views.Grid.GridView gridView1;
+    }
+}
---
+++ DevExpress/XtraForm1.cs
@@ -0,0 +1,89 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevExpress
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+
+            this.Shown += DevForm_Shown;
+        }
+
+        private void DevForm_Shown(object sender, EventArgs e)
+        {
+            InitGridCOntrol();
+
+            this.uiGrid_Main.DataSource = GetData();
+        }
+
+        private DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Grade");
+            dt.Columns.Add("Score");
+
+            dt.Rows.Add(new string[] { "홍길동", "28", "2", "100" });
+            dt.Rows.Add(new string[] { "아무개", "56", "3", "10" });
+            dt.Rows.Add(new string[] { "안정환", "34", "2", "78" });
+            dt.Rows.Add(new string[] { "설기현", "33", "1", "54" });
+            dt.Rows.Add(new string[] { "아이유", "28", "1", "45" });
+            dt.Rows.Add(new string[] { "제시", "23", "2", "10" });
+
+            return dt;
+        }
+
+        private void InitGridCOntrol()
+        {
+            GridView gv = this.uiGrid_Main.MainView as GridView;
+            gv.OptionsView.ShowGroupPanel = false;
+            gv.OptionsBehavior.Editable = false;
+
+            //Row Cell 스타일 이벤트 선언
+            gv.RowCellStyle += DevForm_RowCellStyle;
+        }
+
+        private void DevForm_RowCellStyle(object sender, RowCellStyleEventArgs e)
+        {
+            e.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+
+            GridView view = sender as GridView;
+
+            if (view == null)
+                return;
+
+            if (e.RowHandle != view.FocusedRowHandle)
+            {
+                if (e.Column.Name.Contains("Name"))
+                {
+                    e.Appearance.ForeColor = Color.Blue;
+                }
+                else if (e.Column.Name.Contains("Grade"))
+                {
+                    e.Appearance.ForeColor = Color.Red;
+                }
+            }
+
+            var row = this.gridView1.GetRowCellDisplayText(e.RowHandle, "Name");
+
+            if (row.Equals("홍길동"))
+            {
+                e.Appearance.BackColor = Color.OrangeRed;
+            }
+        }
+    }
+}
